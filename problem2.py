7127e6e55686140ad94aa2cf6ed0b1b8bbc2b147
pranjul2206 <41471782+pranjul2206@users.noreply.github.com>
2022-07-09 00:04:50 +0530

    optimized Q2

--- problem2.py
+++ problem2.py
@@ -2,11 +2,13 @@
 def solve1(n,divisor):
     if n==1:
         return
-    for i in range(divisor,n+1):
+    i=divisor
+    while i<=n:
         if n%i==0:
             print(i,end=' ')
             solve1(n//i,i)
             break
+        i+=1
 
 # using recursion and prime function
 from math import sqrt
@@ -25,10 +27,12 @@ def solve2(n,divisor):
     if isPrime(n):
         print(n,end=' ')
         return
-    for i in range(divisor,n+1):
+    i=divisor
+    while i<=n:
         if n%i==0:
             print(i,end=' ')
             solve2(n//i,i)
             break
+        i+=1
 n=int(input())
 solve2(n,2)
